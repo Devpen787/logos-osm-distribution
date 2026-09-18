929d880042351ca604c81c3b93bca59f5aed3d75
Devpen787 <Devpen787@gmail.com>
2026-09-18 11:24:15 +0200

    refactor(m05): point IDL generator at standard guest path

--- tools/osm-registry-idl/src/main.rs
+++ tools/osm-registry-idl/src/main.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 fn main() {
     let source = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("../../programs/osm-registry/methods/guest/src/bin/osm_registry.rs");
+        .join("../../methods/guest/src/bin/osm_registry.rs");
 
     let idl = spel_framework_core::idl_gen::generate_idl_from_file(&source)
         .unwrap_or_else(|err| panic!("failed to generate OSM registry IDL: {err}"));
