5a7e016b41f465af8ac6a9c4776de2169eb11486
Devpen787 <Devpen787@gmail.com>
2026-09-18 11:06:54 +0200

    feat(m05): generate registry IDL from SPEL source

---
+++ tools/osm-registry-idl/src/main.rs
@@ -0,0 +1,14 @@
+use std::path::PathBuf;
+
+fn main() {
+    let source = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("../../programs/osm-registry/methods/guest/src/bin/osm_registry.rs");
+
+    let idl = spel_framework_core::idl_gen::generate_idl_from_file(&source)
+        .unwrap_or_else(|err| panic!("failed to generate OSM registry IDL: {err}"));
+
+    println!(
+        "{}",
+        serde_json::to_string_pretty(&idl).expect("serialize generated IDL")
+    );
+}
